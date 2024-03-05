f9bd643513189638ece848c0918938f6e4d3f7e1
Guillaume Demerges <guillaume.demerges@protonmail.com>
2024-03-05 20:51:37 +0100

    test sur un fichier json

--- app.py
+++ app.py
@@ -1,21 +1,17 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    with open('message_1.json', 'r') as f:
+        data = json.load(f)
 
-@app.route('/api/statistiques')
-def statistiques():
-    with open('chemin/vers/votre/fichier/messages.json', 'r') as f:
-        messages = json.load(f)
-    stats = calculer_statistiques(messages)
-    return jsonify(stats)
+    stats = calculer_statistiques(data)
+    return render_template('index.html', stats=stats)
 
 def calculer_statistiques(data):
-    # Calcul de statistiques basiques, par exemple, le nombre total de messages
     nombre_messages = len(data['messages'])
     return {
         'nombre_messages': nombre_messages
